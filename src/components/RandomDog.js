e08701c30e4446030750d2d45e2a62780b7cdb3a
ehwus <alex@organise.network>
2022-04-23 11:23:52 +0100

    added a dog picture

--- src/components/RandomDog.js
+++ src/components/RandomDog.js
@@ -5,7 +5,10 @@ export default function RandomDog() {
   return (
     <div className="RandomDog">
       <h2 className="RandomDog-title">Random Dogs</h2>
-      <img className="RandomDog-image" src="http://via.placeholder.com/300x300" />
+      <img
+        className="RandomDog-image"
+        src="https://images.dog.ceo/breeds/airedale/n02096051_3538.jpg"
+      />
       <p>
         <button className="RandomDog-button">Save Image</button>
         <button className="RandomDog-button">Next Dog</button>
